65a6bca7c3723dd025d8c1f30751b34909dcaaf3
Joel Meza Baca <nyxjoel@gmail.com>
2020-05-05 09:39:04 -0500

    custom connection

--- conexion.php
+++ conexion.php
@@ -1,16 +1,16 @@
 <?php
-	
+
     $direccion='127.0.0.1';
     $usuario='root';
     $password='';
-    $bd='bd_sis_eventosacademicos';
+    $bd='eventopj_db';
 	$miconex=new mysqli($direccion,$usuario,$password,$bd);
 	//comprobar conexion
 	if($miconex->connect_errno)
 	{
 		die("Fallo la conexion[".$miconex->connect_error."]");
-	}	
-	
+	}
+
 	function hora_bd($hora)
 	{
 		$hora_bd=date('H:i:s',strtotime($hora));
@@ -21,12 +21,12 @@
 		$fecha_ver=date('d-m-Y',strtotime($fecha));
 		return $fecha_ver;
 	}
-	
+
 	function fecha_mostrar2($fecha)
 	{
 		date_default_timezone_set('America/Lima');
 		setlocale(LC_TIME, 'spanish');
-		
+
 		$fecha_ver2=strftime('%A',strtotime($fecha))." ".date('d',strtotime($fecha)).strftime(' de %B',strtotime($fecha))." del ".date('Y',strtotime($fecha));
 		return $fecha_ver2;
 	}
